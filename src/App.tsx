36f442ea013175ec2e25f225ebeca8e417eb9569
Davidyang2149 <davidyang2149@gmail.com>
2024-12-08 00:27:07 +0900

    🔨 #21 feat: 기록 페이지를 연결하라

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Todos from './pages/Todos.tsx'
-import TodosFind from './pages/TodosFind.tsx'
+import Todos from './pages/Todos'
+import TodosFind from './pages/TodosFind'
 import SurveySpareTime from './pages/SurveySpareTime'
 import Home from './pages/Home'
 import OnboardingPage from './pages/OnboardingPage'
+import TodosMemory from './pages/TodosMemory'
 
 function App() {
   const basename = process.env.NODE_ENV === 'production' ? '/Tickley' : '/'
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/todos" element={<Todos />} />
           <Route path="/todos/find" element={<TodosFind />} />
+          <Route path="/todos/memory" element={<TodosMemory />} />
           <Route path="/survey" element={<SurveySpareTime />} />
           <Route path='/onboarding/:onboardingId' element={<OnboardingPage />} />
         </Routes>
---
+++ src/pages/TodosMemory.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+
+const TodosMemory = () => {
+  return (
+    <div>
+      <p>할일 찾기 페이지입니다.</p>
+      <div>
+        <Link to="/todos">
+          <button>오늘 할일</button>
+        </Link>
+        <Link to="/todos/find">
+          <button>할일 찾기</button>
+        </Link>
+        <Link to="/todos/memory">
+          <button>기록</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default TodosMemory
