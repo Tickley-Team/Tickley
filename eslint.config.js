e44b502253a5158b502b7db0355ccc1ca9ef1611
Hyunsu <hs9880@gmail.com>
2024-12-07 13:05:36 +0900

    chore: vscode setting 추가
    - vscode eslint,prettier 포맷 자동적용설정추가

--- eslint.config.js
+++ eslint.config.js
@@ -7,8 +7,13 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser, ecmaVersion: 'latest' } },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 0,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ]
