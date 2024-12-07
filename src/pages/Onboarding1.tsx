0f37c7996a6aa3a50eb94381b46f22140f21c87d
Davidyang2149 <davidyang2149@gmail.com>
2024-12-07 21:23:38 +0900

    refactor: 라우터 라이브러리를 통일 시켜라

--- src/pages/Onboarding1.tsx
+++ src/pages/Onboarding1.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router'
 
 const Onboarding1 = () => {
   const navigate = useNavigate()
