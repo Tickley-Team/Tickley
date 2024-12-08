3ac1f536315fa85943ab5f2fb5e4a075192fe6f9
Hyunsu <hs9880@gmail.com>
2024-12-08 14:53:08 +0900

    refactor: react-route 6 with loader 라우팅 시스템
    - <BrowserRouter>와 <Routes> 조합에서 createBrowserRouter와 RouterProvider로 리팩터링
    - loader, action, errorElement 등을 함께 관리할 수 있어 라우트 정의가 더 명확
    - todo/find 에 json데이터를 가져오는 로직을 useEffect대신loader를 사용함으로써 컴포넌트렌더링 전에 실행 가능

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+
 import Todos from './pages/Todos'
 import TodosFind from './pages/TodosFind'
 import SurveySpareTime from './pages/SurveySpareTime'
@@ -8,27 +8,60 @@ import OnboardingPage from './pages/OnboardingPage'
 import TodosMemory from './pages/TodosMemory'
 import ProgressTimer from './pages/ProgressTimer'
 import CompletedTimer from './pages/CompletedTimer'
+import { fetchRecommendTodos } from './api/fetchRecommendTodos'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 function App() {
-  const basename = process.env.NODE_ENV === 'production' ? '/Tickley' : '/'
+  const isProduction = process.env.NODE_ENV === 'production'
+  const basename = isProduction ? '/Tickley' : '/'
+
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/todos',
+        element: <Todos />,
+      },
+      {
+        path: '/todos/find',
+        element: <TodosFind />,
+        loader: () =>
+          fetchRecommendTodos(
+            `${import.meta.env.BASE_URL}data/todoRecommendData.json`,
+          ),
+      },
+      {
+        path: '/todos/memory',
+        element: <TodosMemory />,
+      },
+      {
+        path: '/survey',
+        element: <SurveySpareTime />,
+      },
+      {
+        path: '/onboarding/:onboardingId',
+        element: <OnboardingPage />,
+      },
+      {
+        path: '/timer/progress',
+        element: <ProgressTimer />,
+      },
+      {
+        path: '/timer/completed',
+        element: <CompletedTimer />,
+      },
+    ],
+    {
+      basename,
+    },
+  )
 
   return (
     <div className="App">
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todos" element={<Todos />} />
-          <Route path="/todos/find" element={<TodosFind />} />
-          <Route path="/todos/memory" element={<TodosMemory />} />
-          <Route path="/survey" element={<SurveySpareTime />} />
-          <Route
-            path="/onboarding/:onboardingId"
-            element={<OnboardingPage />}
-          />
-          <Route path="/timer/progress" element={<ProgressTimer />} />
-          <Route path="/timer/completed" element={<CompletedTimer />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </div>
   )
 }
--- src/pages/ProgressTimer.tsx
+++ src/pages/ProgressTimer.tsx
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import ProgressTimerQuitPopup from "../components/ProgressTimerQuitPopup";
+import ProgressTimerQuitPopup from '../components/ProgressTimerQuitPopup'
 
 const ProgressTimer = () => {
-  const [isShowQuitPopup, setShowQuitPopup] = useState(false);
+  const [isShowQuitPopup, setShowQuitPopup] = useState(false)
   const handleClickTimeStop = () => {
     // TODO: 시간 멈추기
   }
-  return(
+  return (
     <>
       <div>
         <h1>15분 명상하기</h1>
-        <p>자투리시간 15분간 <br/>몸과 마음을 리프레시 해보아요</p>
+        <p>
+          자투리시간 15분간 <br />
+          몸과 마음을 리프레시 해보아요
+        </p>
         <div>시간 영역</div>
         <div>프로그레스바</div>
         <button onClick={handleClickTimeStop}>일시정지</button>
         <button onClick={() => setShowQuitPopup(true)}>그만두기</button>
       </div>
-      {isShowQuitPopup && <ProgressTimerQuitPopup onClickClose={() => setShowQuitPopup(false)} />}
+      {isShowQuitPopup && (
+        <ProgressTimerQuitPopup onClickClose={() => setShowQuitPopup(false)} />
+      )}
     </>
-  );
+  )
 }
 
-export default ProgressTimer;
+export default ProgressTimer
