19a3c790767125cbd14b0872e6dd00277d6e0a79
zoe <ddu_buk_e@naver.com>
2024-12-08 14:38:29 +0900

    modify: 서브버튼 활성화 시 메인버튼이 +에서 x로 전환

--- src/components/AddTodoMainButton.tsx
+++ src/components/AddTodoMainButton.tsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 import AddTodoSubButton from './AddTodoSubButton'
 
 const AddTodoMainButton = () => {
-    const [showSubButtons, setShowSubButtons] = useState(false)
+  const [showSubButtons, setShowSubButtons] = useState(false)
 
-    const clickMainButton = () => {
-        setShowSubButtons((prev) => !prev)
-    }
+  const clickMainButton = () => {
+    setShowSubButtons((prev) => !prev)
+  }
 
-    return (
-        <div>
-            <button onClick={clickMainButton}>+</button>
-            {showSubButtons && <AddTodoSubButton />}
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={clickMainButton}>{showSubButtons ? 'x' : '+'}</button>
+      {showSubButtons && <AddTodoSubButton />}
+    </div>
+  )
 }
 
 export default AddTodoMainButton
