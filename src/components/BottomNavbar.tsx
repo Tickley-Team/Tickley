30e987b9d4fb630269686393e499d3110e4df004
Davidyang2149 <davidyang2149@gmail.com>
2024-12-09 20:20:11 +0900

    feat: 네비바 UI 클릭 처리를  추가하라

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -1,20 +1,39 @@
-import {Link} from 'react-router'
+import { Link, useLocation } from 'react-router';
+
 import './BottomNavbar.css'
 
 const BottomNavbar = () => {
-    return (
-        <div className="bottom-navbar" style={{ marginBottom: '13px', backgroundColor: '#FFFFFF' }}>
-          <Link to="/todos">
-            <img src='/src/assets/navbar/today-todos-click.svg' alt='오늘 할일' />
-          </Link>
-          <Link to="/todos/find">
-            <img src='/src/assets/navbar/find-todos-click.svg' alt='할일 찾기' />
-          </Link>
-          <Link to="/todos/memory">
-            <img src='/src/assets/navbar/todos-memory-click.svg' alt='기록' />
-          </Link>
-        </div>
-    )
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  return (
+    <div className="bottom-navbar" style={{ marginBottom: '13px', backgroundColor: '#FFFFFF' }}>
+      <Link to="/todos">
+        <img 
+          src={pathname === '/todos' 
+            ? '/src/assets/navbar/today-todos-click.svg'
+            : '/src/assets/navbar/today-todos-unclick.svg'} 
+          alt='오늘 할일' 
+        />
+      </Link>
+      <Link to="/todos/find">
+        <img 
+          src={pathname === '/todos/find'
+            ? '/src/assets/navbar/find-todos-click.svg'
+            : '/src/assets/navbar/find-todos-unclick.svg'} 
+          alt='할일 찾기' 
+        />
+      </Link>
+      <Link to="/todos/memory">
+        <img 
+          src={pathname === '/todos/memory'
+            ? '/src/assets/navbar/todos-memory-click.svg'
+            : '/src/assets/navbar/todos-memory-unclick.svg'} 
+          alt='기록' 
+        />
+      </Link>
+    </div>
+  )
 }
 
 export default BottomNavbar
