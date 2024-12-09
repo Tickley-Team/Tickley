47441f1847d489704eb656870bb5de6c9d1d31cf
Hyunsu <hs9880@gmail.com>
2024-12-09 20:59:42 +0900

    feat: 하단 메뉴 아이콘 추가

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -1,35 +1,39 @@
-import { Link, useLocation } from 'react-router';
-
+import { Link, useLocation } from 'react-router'
+import todayTodosClick from '../assets/navbar/today-todos-click.svg'
+import todayTodosUnClick from '../assets/navbar/today-todos-unclick.svg'
+import findTodoClick from '../assets/navbar/find-todos-click.svg'
+import findTodoUnClick from '../assets/navbar/find-todos-unclick.svg'
+import todoMemoryClick from '../assets/navbar/todos-memory-click.svg'
+import todoMemoryUnClick from '../assets/navbar/todos-memory-unclick.svg'
 import './BottomNavbar.css'
 
 const BottomNavbar = () => {
-  const location = useLocation();
-  const pathname = location.pathname;
+  const location = useLocation()
+  const pathname = location.pathname
 
   return (
-    <div className="bottom-navbar" style={{ marginBottom: '13px', backgroundColor: '#FFFFFF' }}>
+    <div
+      className="bottom-navbar"
+      style={{ marginBottom: '13px', backgroundColor: '#FFFFFF' }}
+    >
       <Link to="/todos">
-        <img 
-          src={pathname === '/todos' 
-            ? '/src/assets/navbar/today-todos-click.svg'
-            : '/src/assets/navbar/today-todos-unclick.svg'} 
-          alt='오늘 할일' 
+        <img
+          src={pathname === '/todos' ? todayTodosClick : todayTodosUnClick}
+          alt="오늘 할일"
         />
       </Link>
       <Link to="/todos/find">
-        <img 
-          src={pathname === '/todos/find'
-            ? '/src/assets/navbar/find-todos-click.svg'
-            : '/src/assets/navbar/find-todos-unclick.svg'} 
-          alt='할일 찾기' 
+        <img
+          src={pathname === '/todos/find' ? findTodoClick : findTodoUnClick}
+          alt="할일 찾기"
         />
       </Link>
       <Link to="/todos/memory">
-        <img 
-          src={pathname === '/todos/memory'
-            ? '/src/assets/navbar/todos-memory-click.svg'
-            : '/src/assets/navbar/todos-memory-unclick.svg'} 
-          alt='기록' 
+        <img
+          src={
+            pathname === '/todos/memory' ? todoMemoryClick : todoMemoryUnClick
+          }
+          alt="기록"
         />
       </Link>
     </div>
@@ -37,4 +41,3 @@ const BottomNavbar = () => {
 }
 
 export default BottomNavbar
-
